c74cd41babd8e8646b111d8f8d1b576786294525
Omark4321 <112348505+Omark4321@users.noreply.github.com>
2026-08-16 16:37:22 -0700

    Add new YouTube link to content.js

--- content.js
+++ content.js
@@ -8,6 +8,7 @@ const videos = [
   "https://youtu.be/vCeMJNWTeWo",
   "https://youtu.be/HK9FWOXDS28",
   "https://youtu.be/LdIwiwQkcw8",
+  "https://youtu.be/cOns2ZazVYM",
 ];
 
 // Put image files inside the photos/ folder, then list their filenames here.
