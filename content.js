806f8ddad4cca064178c3688638f88a55ed8e90a
Omark4321 <112348505+Omark4321@users.noreply.github.com>
2026-08-28 07:49:20 -0700

    Update content.js

--- content.js
+++ content.js
@@ -10,11 +10,12 @@ const videos = [
   "https://youtu.be/LdIwiwQkcw8",
   "https://youtu.be/cOns2ZazVYM",
   "https://youtu.be/jxkQFMgUacE",
+  "https://youtu.be/f-AyN-04Q1Q",
+  "https://youtu.be/NkgiJktlLPM"'
+  "https://youtu.be/1WuRzb9hp6U",
 ];
 
 // Put image files inside the photos/ folder, then list their filenames here.
 const photos = [
-  "https://youtu.be/f-AyN-04Q1Q",
-  "https://youtu.be/NkgiJktlLPM"'
-  "https://youtu.be/1WuRzb9hp6U",
+  // put photos
 ];
